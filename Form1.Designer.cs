e0b2a253d13e73145ec0a73ae71bec6f9bd640c7
Yan <yvk36@tpu.ru>
2023-12-07 01:29:45 +0700

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace ICS_Lab_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 142);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(220, 150);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 123);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Text to encrypt:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(241, 142);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(219, 150);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(241, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Text to decrypt:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 298);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(220, 33);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Encrypt text";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(241, 298);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(219, 33);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Decrypt text";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(15, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(285, 49);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Encryption method";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(115, 21);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(83, 20);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Vigenere";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 21);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(64, 20);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Cesar";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(15, 98);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(124, 22);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(145, 98);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(124, 22);
+            this.textBox4.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 16);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Position";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(142, 76);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 16);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Key";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(363, 353);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(475, 22);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(362, 465);
+            this.textBox5.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(997, 540);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ICS_Lab_3
+{
+    
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = Data.Cesar_with_key(textBox1.Text, Convert.ToInt16(textBox3.Text), textBox4.Text, true);
+            textBox1.Text = "";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = Data.Cesar_with_key(textBox2.Text, Convert.ToInt16(textBox3.Text), textBox4.Text, false);
+            textBox2.Text = "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Data.Vigenere("Key", "Key", textBox5);
+        }
+    }
+
+    public static class Data
+    {
+        public static string alphabet_h = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        public static string alphabet_l = alphabet_h.ToLower();
+        public static string alphabet_s = " 1234567890";
+        public static string alphabet_common = alphabet_h + alphabet_l + alphabet_s;
+        public static string key_word = "";
+        public static string common = "";
+        public static int index;
+        public static string Cesar_with_key(string to_encrypt, int position, string key_word, bool mode)
+        {
+            string encrypted = "";
+            string alph = alphabet_h + alphabet_l + alphabet_s;
+            string alph_b = alph;
+            string before_key = "";
+            //Deleting key letters from alphabet
+            for (int i = 0; i < alph.Length; i++)
+            {
+                for (int j = 0; j < key_word.Length; j++)
+                {
+                    if (alph[i] == key_word[j])
+                    {
+                        alph = alph.Remove(i, 1);
+                    }
+                }
+            }
+            //Seacring letters to set before key word (shift)
+            for (int k = alph.Length - position; k < alph.Length; k++)
+            {
+                before_key += alph[k];
+            }
+            //Deleting shifted letters from alphabet
+            for (int i = 0; i < alph.Length; i++)
+            {
+                for (int j = 0; j < before_key.Length; j++)
+                {
+                    if (alph[i] == before_key[j])
+                    {
+                        alph = alph.Remove(i, 1);
+                    }
+                }
+            }
+            //Collecting parts of the key string
+            common = before_key + key_word + alph;
+            //Now common is key, alph_b is alphabet
+            if (mode)
+            {
+                for (int n = 0; n < to_encrypt.Length; n++)
+                {
+                    for (int m = 0; m < alph_b.Length; m++)
+                    {
+                        if (to_encrypt[n] == alph_b[m])
+                        {
+                            encrypted += common[m];
+                        }
+                    }
+                }
+            }
+            else
+            {
+                for (int n = 0; n < to_encrypt.Length; n++)
+                {
+                    for (int m = 0; m < common.Length; m++)
+                    {
+                        if (to_encrypt[n] == common[m])
+                        {
+                            encrypted += alph_b[m];
+                        }
+                    }
+                }
+            }
+            return encrypted;
+        }
+        public static string Vigenere(string to_encrypt, string slogan, TextBox t1)
+        {
+            TextBox tb = t1;
+            string[] vigenere_matrix = new string[alphabet_h.Length];
+            string encrypted = "1";
+            string shift(string to_shift, int step)
+            {
+                string after = "";
+                string before = "";
+                for (int i = 0; i < step; i++)
+                {
+                    after += to_shift[i];
+                }
+                before = to_shift.Remove(0, step);
+                return before + after;
+            }
+            //Matrix filling
+            for (int i = 0; i < vigenere_matrix.Length; i++)
+            {
+                tb.AppendText(shift(alphabet_h, i) + '\n');
+            }
+
+
+
+            return encrypted;
+        }
+    }
+}
